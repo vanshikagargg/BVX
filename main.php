3f77f4afcf9e5de03c56011accd359756c486ebf
Shubhi Singh <67328331+shubhisingh184@users.noreply.github.com>
2022-04-10 03:35:57 +0530

    Update and rename main.html to main.php

---
+++ main.php
@@ -0,0 +1,57 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>BSU</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <style>
+        body {
+          background-image: url('pic.jpeg');
+        }
+        </style>
+
+    <div class="main">
+        <div class="navbar">
+            <div class="icon">
+                <h2 class="logo">BVX</h2>
+            </div>
+
+            <div class="menu">
+                <ul>
+                    <li class="active"><a href="main.php">HOME</a></li>
+                    <li><a href="about.html">ABOUT</a></li>
+                    <li><a href="#">SERVICES</a>
+                        <div class="sub-menu-1">
+                            <ul>
+                                <li><a href="buy.html">BUY</a></li>
+                                <li><a href="sell.html">SELL</a></li>
+                                <li><a href="sell.html">RENT</a></li>
+                            </ul>
+                        </div>
+                    </li>
+                    <li><a href="contact.html">CONTACT</a></li>
+                    <li><a href="profile.html">PROFILE</a></li>
+                
+            </div>
+         </div> 
+        <div class="content">
+            <h1>BUY / SELL <br><span>OR RENT</span> <br>STUFF</h1>
+            <p class="par">Immerse yourself into ease of exchanging</p>
+                
+                
+       
+        
+    </div>
+    <script src="https://unpkg.com/ionicons@5.4.0/dist/ionicons.js"></script>
+    <script src='https://code.jquery.com/jquery-3.6.0.min.js'>
+
+    </script>
+    <script>
+        $('.message a').click(function(){
+            $('form').animate({height: "toggle",opacity: "toggle"}, "slow");
+        });
+    </script>
+</body>
+</html>
